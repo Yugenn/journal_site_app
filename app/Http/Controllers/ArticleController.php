205cba5b1247447bfd2eeb9d3a1d6612d8563c14
Yugenn <yuruseajyan@gmail.com>
2021-09-21 11:58:05 +0900

    課題2完了

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -20,4 +20,25 @@ class ArticleController extends Controller
         // Articlesティレクトリーの中のindexページを指定し、itemsの連想配列を代入
         return view('articles.index', ['articles' => $articles]);
     }
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $article = new Article;
+
+        // 値の用意
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+
+        // インスタンスに値を設定して保存
+        $article->save();
+
+        // 登録したらindexに戻る
+        return redirect('/articles');
+    }
 }
